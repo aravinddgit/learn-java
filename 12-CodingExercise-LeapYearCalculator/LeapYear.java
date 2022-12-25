65cee752b2a0a1b26e54d832a19662c3574a1455
Aravind Dinakaran <aravinddmail@gmail.com>
2022-12-25 12:47:17 +0530

    1. Adding 1 coding exercise - Leap Year Calculator

---
+++ 12-CodingExercise-LeapYearCalculator/LeapYear.java
@@ -0,0 +1,23 @@
+public class LeapYear {
+    public static void main(String[] args) {
+        System.out.println(isLeapYear(-1600));
+        System.out.println(isLeapYear(1600));
+        System.out.println(isLeapYear(2017));
+        System.out.println(isLeapYear(2000));
+    }
+
+    public static boolean isLeapYear(int year){
+        if(year < 1 || year > 9999) return false;
+        boolean isLeap = false;
+        if((year % 4) == 0){
+            if((year % 100) == 0){
+                if((year % 400) == 0){
+                    isLeap = true;
+                }
+            }else{
+                isLeap = true;
+            }
+        }
+        return isLeap;
+    }
+}
